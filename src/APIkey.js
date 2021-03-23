48ed803b3129d8ed8683437383103351b5349aff
vprasadreddy <reddy.rulz92@gmail.com>
2021-03-23 07:41:35 -0400

    Delete APIkey.js

--- src/APIkey.js
+++
@@ -1,7 +0,0 @@
-/* Sample API URL
-const APIURL =
-  "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"; */
-const APP_ID = "893e9059";
-const APP_KEY = "b383b9aba005399d5404972c13b740bf";
-
-export { APP_ID, APP_KEY };
