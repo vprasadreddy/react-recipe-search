137ae7c8999caafc3c21379e4d3a0226b1e37c6c
vprasadreddy <reddy.rulz92@gmail.com>
2021-04-10 20:43:12 -0400

    added on hover shadow effect

--- src/LoadingSpinner.js
+++ src/LoadingSpinner.js
@@ -1,16 +1,10 @@
+import "./App.css";
 import React from "react";
 import { Spinner } from "react-bootstrap";
 
 function LoadingSpinner() {
-  const spinnerCSS = {
-    position: "fixed",
-    top: "50%",
-    left: "50%",
-    /* to position the div exactly at center */
-    transform: "translate(-50%, -50%)"
-  };
   return (
-    <div style={spinnerCSS}>
+    <div className="loading-spinner">
       <Spinner animation="border" role="status" variant="dark">
         <span className="sr-only">Loading...</span>
       </Spinner>
